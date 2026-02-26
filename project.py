5a7956dfe87f20ef4a74f21bfafac8af1ebcb706
Tareef-debugger <tareeftoufique@gmail.com>
2026-02-26 16:44:34 +0600

    Add files via upload

---
+++ project.py
@@ -0,0 +1,27 @@
+import numpy as np 
+
+a = np.arange(9,dtype=np.float64).reshape(3,3)
+print('First Array')
+print(a)
+print('\n')
+
+b= np.array([10,10,10])
+print('Second array:')
+print(b)
+print('\n')
+
+print('add the two arrays:')
+print(np.add(a,b))
+print('\n')
+
+print('subtract the two arrays:')
+print(np.subtract(a,b))
+print('\n')
+
+print('multiply the two arrays:')
+print(np.multiply(a,b))
+print('\n')
+
+print('divide the two arrays:')
+print(np.divide(a,b))
+print('\n')
